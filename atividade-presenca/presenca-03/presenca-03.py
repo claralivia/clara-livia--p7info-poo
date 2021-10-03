3f6358ddbee7c28f4c5d4b6eaf212a38d099b2c0
claralivia <89024675+claralivia@users.noreply.github.com>
2021-10-03 11:46:32 -0300

    Update presenca-03.py

--- atividade-presenca/presenca-03/presenca-03.py
+++ atividade-presenca/presenca-03/presenca-03.py
@@ -1,32 +1,20 @@
-<<<<<<< HEAD
 class Pilha:
   def __init__(self):
       self.element = list()
-
   def inserir(self,name):
       self.element.append(name)
       print(f'Inserindo o elemento {name}: ' + ' '.join(self.element))
-=======
-pilha = list()
-print("Pilha: ", pilha)
-
-def inserir(nome):
-    pilha.append(nome)
-    print(f'\nInserindo o elemento {nome}: ' + ' '.join(pilha))
->>>>>>> 9ad93867ac890597e55c4373d775a1c8144f5e43
-
   def remover(self):
       self.element.pop()
       print(f'Removendo o último elemento: ' + ' '.join(self.element))
-
   def exibir(self):
       print('Pilha: ' + ' '.join(self.element))
 
-fila = Pilha()
-fila.inserir('apple')
-fila.inserir('grape')
-fila.inserir('lemon')
+pilha = Pilha()
+pilha.inserir('apple')
+pilha.inserir('grape')
+pilha.inserir('lemon')
 
-fila.remover()
+pilha.remover()
 
-fila.exibir()
+pilha.exibir()
