45ccc7deb35ff5d92605bfaaf3c23e954a013bec
claralivia <89024675+claralivia@users.noreply.github.com>
2021-10-03 11:48:10 -0300

    POO

--- atividade-presenca/presenca-04/presenca-04.py
+++ atividade-presenca/presenca-04/presenca-04.py
@@ -1,15 +1,13 @@
+# -*- coding: utf-8 -*-
 class Fila:
   def __init__(self):
       self.element = list()
-
   def inserir(self,name):
       self.element.append(name)
       print(f'Inserindo o elemento {name}: ' + ' '.join(self.element))
-
   def remover(self):
       self.element.pop(0)
       print(f'Removendo o primeiro elemento: ' + ' '.join(self.element))
-
   def exibir(self):
       print('Fila: ' + ' '.join(self.element))
 
