d8781c77ee1b9fd8f765cba940eeb616761e8a3f
unknown <clara.livia.moura62@aluno.ifce.edu.br>
2021-10-03 11:44:00 -0300

    POO

--- atividade-presenca/presenca-04/presenca-04.py
+++ atividade-presenca/presenca-04/presenca-04.py
@@ -1,4 +1,4 @@
-class Pilha:
+class Fila:
   def __init__(self):
       self.element = list()
 
@@ -7,13 +7,13 @@ class Pilha:
       print(f'Inserindo o elemento {name}: ' + ' '.join(self.element))
 
   def remover(self):
-      self.element.pop()
-      print(f'Removendo o último elemento: ' + ' '.join(self.element))
+      self.element.pop(0)
+      print(f'Removendo o primeiro elemento: ' + ' '.join(self.element))
 
   def exibir(self):
-      print('Pilha: ' + ' '.join(self.element))
+      print('Fila: ' + ' '.join(self.element))
 
-fila = Pilha()
+fila = Fila()
 fila.inserir('apple')
 fila.inserir('grape')
 fila.inserir('lemon')
